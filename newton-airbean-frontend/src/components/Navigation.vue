2a48dec8b38025f16d8d0807b0df5c6dc1e1fa18
thomascarleberg <thomascarleberg@gmail.com>
2021-02-05 10:59:31 +0100

    Frontend stuffs

--- newton-airbean-frontend/src/components/Navigation.vue
+++ newton-airbean-frontend/src/components/Navigation.vue
@@ -1,18 +1,31 @@
 <template>
-  <nav>
-    <!-- Top small -->
-    <div>
-        
-    </div>
+  <nav class="navigation">
     <!-- Cover modal -->
-    <div class="">
+    <div v-if="showMenu" class="large">
       <router-link to="/menu">Meny</router-link>
       <router-link to="/about">Vårt kaffe</router-link>
       <router-link to="/profile">Min profil</router-link>
       <router-link to="/status">Orderstatus</router-link>
+      <button class="hamburger-circle" @click="showMenu=!showMenu">
+          <img src="~@/assets/close.svg" alt="Navigate" />
+        </button>
       <div>
         <router-link to="/cart">Kassa({{ numberOfItemsInCart }})</router-link>
       </div>
+    </div>
+    <!-- Top small -->
+    <div v-else class="small">
+        <button class="hamburger-circle" @click="showMenu=!showMenu">
+          <img src="~@/assets/navicon.svg" alt="Navigate" />
+        </button>
+        <router-link to="/cart"
+          >
+          <div class="round-button relative">
+            <div class="red-round-circle top-right absolute center-text">
+            {{ numberOfItemsInCart }}
+            </div>
+          </div>
+        </router-link>
     </div>
   </nav>
 </template>
@@ -22,6 +35,7 @@ export default {
   data() {
     return {
       cartItemCount: -1,
+      showMenu: false,
     };
   },
   computed: {
